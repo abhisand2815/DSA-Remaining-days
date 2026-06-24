58ed4b746ee0459754d5cd83363c0a3a576a0399
Abhmanyu Singh <abhimanyu.24bce10726@vitbhopal.ac.in>
2026-06-24 10:09:06 +0530

    Time: 15 ms (92.74%), Space: 98.2 MB (28.08%) - LeetHub

---
+++ 0077-combinations/0077-combinations.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<List<Integer>> combine(int n, int k) {
+
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack(1, n, k, new ArrayList<>(), result);
+        return result;
+    }
+
+    private void backtrack(int start, int n, int k,
+                           List<Integer> path,
+                           List<List<Integer>> result) {
+
+        if (path.size() == k) {
+            result.add(new ArrayList<>(path));
+            return;
+        }
+
+        // 🔥 pruning condition
+        for (int i = start; i <= n - (k - path.size()) + 1; i++) {
+            path.add(i);
+            backtrack(i + 1, n, k, path, result);
+            path.remove(path.size() - 1);
+        }
+    }
+}
