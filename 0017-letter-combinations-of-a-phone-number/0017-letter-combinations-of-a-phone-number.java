6df4aa4eee7456ff8738199a9fe516eabfffb8ba
Abhmanyu Singh <abhimanyu.24bce10726@vitbhopal.ac.in>
2026-06-25 09:09:34 +0530

    Time: 0 ms (100%), Space: 43.7 MB (41.09%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -2,41 +2,38 @@ import java.util.*;
 
 class Solution {
 
+    private static final String[] MAP = {
+        "", "", "abc", "def", "ghi",
+        "jkl", "mno", "pqrs", "tuv", "wxyz"
+    };
+
     public List<String> letterCombinations(String digits) {
 
         List<String> result = new ArrayList<>();
 
-        if (digits == null || digits.length() == 0) {
+        if (digits == null || digits.length() == 0)
             return result;
-        }
 
-        String[] map = {
-            "", "",
-            "abc", "def",
-            "ghi", "jkl",
-            "mno", "pqrs",
-            "tuv", "wxyz"
-        };
+        char[] path = new char[digits.length()];
 
-        Queue<String> queue = new LinkedList<>();
-        queue.offer("");
+        backtrack(digits, 0, path, result);
 
-        for (char digit : digits.toCharArray()) {
-
-            int size = queue.size();
-            String letters = map[digit - '0'];
-
-            for (int i = 0; i < size; i++) {
-
-                String current = queue.poll();
-
-                for (char c : letters.toCharArray()) {
-                    queue.offer(current + c);
-                }
-            }
-        }
-
-        result.addAll(queue);
         return result;
+    }
+
+    private void backtrack(String digits, int index,
+                           char[] path, List<String> result) {
+
+        if (index == digits.length()) {
+            result.add(new String(path));
+            return;
+        }
+
+        String letters = MAP[digits.charAt(index) - '0'];
+
+        for (int i = 0; i < letters.length(); i++) {
+            path[index] = letters.charAt(i);
+            backtrack(digits, index + 1, path, result);
+        }
     }
 }
