4df9b9cb0c79fd514bad55f8525ca41e0a41ff5e
Abhmanyu Singh <abhimanyu.24bce10726@vitbhopal.ac.in>
2026-07-13 10:39:24 +0530

    Time: 1 ms (100%), Space: 48.8 MB (71.82%) - LeetHub

---
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -0,0 +1,72 @@
+class Solution {
+
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+
+        // Always binary search on smaller array
+        if (nums1.length > nums2.length) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
+
+        int m = nums1.length;
+        int n = nums2.length;
+
+        int left = 0;
+        int right = m;
+
+        while (left <= right) {
+
+            int partitionX = left + (right - left) / 2;
+            int partitionY = (m + n + 1) / 2 - partitionX;
+
+            int maxLeftX =
+                    (partitionX == 0)
+                    ? Integer.MIN_VALUE
+                    : nums1[partitionX - 1];
+
+            int minRightX =
+                    (partitionX == m)
+                    ? Integer.MAX_VALUE
+                    : nums1[partitionX];
+
+            int maxLeftY =
+                    (partitionY == 0)
+                    ? Integer.MIN_VALUE
+                    : nums2[partitionY - 1];
+
+            int minRightY =
+                    (partitionY == n)
+                    ? Integer.MAX_VALUE
+                    : nums2[partitionY];
+
+            // Correct partition found
+            if (maxLeftX <= minRightY &&
+                maxLeftY <= minRightX) {
+
+                // Even length
+                if ((m + n) % 2 == 0) {
+
+                    return (
+                            Math.max(maxLeftX, maxLeftY)
+                            +
+                            Math.min(minRightX, minRightY)
+                           ) / 2.0;
+                }
+
+                // Odd length
+                return Math.max(maxLeftX, maxLeftY);
+            }
+
+            // Move left
+            else if (maxLeftX > minRightY) {
+                right = partitionX - 1;
+            }
+
+            // Move right
+            else {
+                left = partitionX + 1;
+            }
+        }
+
+        return 0.0;
+    }
+}
