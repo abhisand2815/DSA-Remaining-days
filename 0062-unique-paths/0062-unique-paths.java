28619aaeddfac7a2e026242c88895db5a89b6957
Abhmanyu Singh <abhimanyu15282005@gmail.com>
2026-09-16 17:37:58 +0530

    Time: 0 ms (100%), Space: 42.3 MB (12.8%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,14 +1,32 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        int[] dp = new int[n];
-        java.util.Arrays.fill(dp, 1);
 
+        // Create a 2D array to store the number of paths to each cell
+        int[][] dp = new int[m][n];
+
+        // There is only 1 way to reach any cell in the first row
+        // (moving right)
+        for (int i = 0; i < m; i++) {
+            dp[i][0] = 1;
+        }
+
+        // There is only 1 way to reach any cell in the first column
+        // (moving down)
+        for (int j = 0; j < n; j++) {
+            dp[0][j] = 1;
+        }
+
+        // Fill the rest of the grid
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
-                dp[j] += dp[j - 1];
+
+                // Total paths to current cell =
+                // paths from above + paths from left
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
             }
         }
 
-        return dp[n - 1];
+        // Return the value at the bottom-right corner
+        return dp[m - 1][n - 1];
     }
 }
